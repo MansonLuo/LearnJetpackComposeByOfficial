0f76450dd9b15efe555583ed1fc851d0c81b3f33
MansonLuo <1558911620@qq.com>
2023-09-23 22:48:45 +0800

    saved.

--- app/src/main/java/com/example/learnjetpackcomposebyofficial/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcomposebyofficial/MainActivity.kt
@@ -3,13 +3,31 @@ package com.example.learnjetpackcomposebyofficial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learnjetpackcomposebyofficial.ui.theme.LearnJetpackComposeByOfficialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +40,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,17 +47,93 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun Titles(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-    )
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = null
+        )
+        Text(
+            text = "Manson Luo",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = "Android Development Extraordinaire",
+            color = Color(0xFF3ddc84)
+        )
+    }
+}
+
+@Composable
+fun Contacts(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                Icons.Filled.Call,
+                contentDescription = "",
+            )
+            Text(text = "+11 (123) 444 555 666")
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 8.dp, bottom = 8.dp),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                Icons.Filled.Share,
+                contentDescription = null
+            )
+            Text(text = "@AndroidDev")
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                Icons.Filled.Email,
+                contentDescription = null
+            )
+            Text(
+                text = "jen.doe@android.com"
+            )
+        }
+    }
+}
+
+@Composable
+fun BusinessCard() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = Color(0xffd9e8d8)
+            )
+            .padding(bottom = 16.dp)
+    ) {
+        Titles(
+            modifier = Modifier.weight(1f)
+        )
+        Contacts()
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LearnJetpackComposeByOfficialTheme {
-        Greeting("Android")
-    }
+fun AppPreview() {
+    BusinessCard()
 }
