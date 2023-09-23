e503cf58a66a968a575b7952dacd165177abbd1b
MansonLuo <1558911620@qq.com>
2023-09-23 19:57:51 +0800

    finished.

--- app/src/main/java/com/example/learnjetpackcomposebyofficial/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcomposebyofficial/MainActivity.kt
@@ -3,13 +3,22 @@ package com.example.learnjetpackcomposebyofficial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learnjetpackcomposebyofficial.ui.theme.LearnJetpackComposeByOfficialTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TaskComplete()
                 }
             }
         }
@@ -30,17 +39,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun TaskComplete(
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_task_completed),
+            contentDescription = ""
+        )
+        Text(
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+            text = "All tasks completed",
+            fontWeight = FontWeight.Bold,
+
+        )
+        Text(
+            text = "Nice work!",
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LearnJetpackComposeByOfficialTheme {
-        Greeting("Android manson")
-    }
+fun TaskCompletedPreview() {
+    TaskComplete()
 }
