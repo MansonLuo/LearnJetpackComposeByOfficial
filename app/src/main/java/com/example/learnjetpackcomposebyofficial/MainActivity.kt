57d9ad9f1758e0619a951b8fc228479a641aa632
MansonLuo <1558911620@qq.com>
2023-09-23 18:10:03 +0800

    saving.

--- app/src/main/java/com/example/learnjetpackcomposebyofficial/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcomposebyofficial/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.learnjetpackcomposebyofficial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learnjetpackcomposebyofficial.ui.theme.LearnJetpackComposeByOfficialTheme
 
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Article()
                 }
             }
         }
@@ -30,17 +32,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun Article(
+    modifier: Modifier = Modifier
+) {
+    Image(
+        painter = painterResource(id = R.drawable.bg_compose_background),
+        contentDescription = ""
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LearnJetpackComposeByOfficialTheme {
-        Greeting("Android")
-    }
+fun ArticlePreview() {
+    Article()
 }
