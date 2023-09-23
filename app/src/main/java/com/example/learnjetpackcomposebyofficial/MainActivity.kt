1b3a90f184e93741361b5c5d35edd9c22bb24e12
MansonLuo <1558911620@qq.com>
2023-09-23 19:09:22 +0800

    saving

--- app/src/main/java/com/example/learnjetpackcomposebyofficial/MainActivity.kt
+++ app/src/main/java/com/example/learnjetpackcomposebyofficial/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LearnJetpackComposeByOfficialTheme {
-        Greeting("Android")
+        Greeting("Android manson")
     }
 }
