e4f0918f343ab5c5d28ee59af4340540c9371ef0
MansonLuo <1558911620@qq.com>
2023-09-24 15:43:13 +0800

    saved.

--- app/src/main/java/com/example/learnjetpackcomposebyofficial/WellnessTaskItem.kt
+++ app/src/main/java/com/example/learnjetpackcomposebyofficial/WellnessTaskItem.kt
@@ -4,10 +4,16 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -16,6 +22,8 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WellnessTaskItem(
     taskName: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
     onClose: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -29,15 +37,33 @@ fun WellnessTaskItem(
                 .weight(1f)
                 .padding(start = 16.dp)
         )
-        
+        Checkbox(
+            checked = checked,
+            onCheckedChange = onCheckedChange
+        )
         IconButton(onClick = onClose) {
             Icon(Icons.Filled.Close, contentDescription = "Close")
         }
     }
 }
 
+@Composable
+fun WellnessTaskItem(taskName: String, modifier: Modifier = Modifier) {
+    var checkedState by rememberSaveable {
+        mutableStateOf(false)
+    }
+
+    WellnessTaskItem(
+        taskName = taskName,
+        checked = checkedState,
+        onCheckedChange = { newValue -> checkedState = newValue},
+        onClose = { },
+        modifier = modifier,
+    )
+}
+
 @Preview(showBackground = true)
 @Composable
 fun WellnessTaskItemPreview() {
-    WellnessTaskItem(taskName = "This is a task", onClose = { /*TODO*/ })
+    WellnessTaskItem(taskName = "This is a task", checked = true, onCheckedChange = {}, onClose = { /*TODO*/ })
 }
---
+++ app/src/main/java/com/example/learnjetpackcomposebyofficial/WellnessTaskList.kt
@@ -0,0 +1,25 @@
+package com.example.learnjetpackcomposebyofficial
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Modifier
+
+
+
+@Composable
+fun WellnessTaskList(
+    modifier: Modifier = Modifier,
+    list: SnapshotStateList<WellnessTask>,
+    onCloseTask: (WellnessTask) -> Unit
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(list) {task ->
+            WellnessTaskItem(taskName = task.label)
+        }
+    }
+}
